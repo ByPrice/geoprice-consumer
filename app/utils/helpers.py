02ed3727362be61a9898220fd3c99eab57e8c75d
jorgeviz <javg44@hotmail.com>
2018-05-11 17:54:56 -0500

    Compare history

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,8 +1,10 @@
 import datetime
 import pandas as pd
+import numpy as np
 import requests
 from config import *
 from app import logger
+import calendar
 
 geo_stores_url = SRV_GEOLOCATION+'/store/retailer?key=%s'
 geo_rets_url = SRV_GEOLOCATION+'/retailer/all'
@@ -82,3 +84,113 @@ def get_all_stores(rets=[]):
     stores_df['store_uuid'] = stores_df['uuid'].astype(str)
     del stores_df['uuid']
     return stores_df
+
+def obtain_distances(fixed, added, rets):
+    """ Obtain distances from all stores
+
+        Params:
+        -----
+        fixed : str
+            Store UUID from Fixed 
+        added : list
+            Store UUIDs from Added
+        - rets : List
+            Retailers 
+
+        Returns:
+        -----
+        _added_d : dict
+            Hash table mapping fixed store to added store
+    """
+    # Fetch all stores
+    _st_list = []
+    _geo_df = get_all_stores([{'key': r} for r in rets])
+    # Get fixed store values
+    _f =  _geo_df[_geo_df['store_uuid'] == fixed]
+    f_lat, f_lng = _f['lat'].values[0], _f['lng'].values[0]
+    # Compute distances from fixed against added
+    _geo_df = _geo_df[_geo_df.store_uuid.isin(added)]
+    _geo_df['fdist'] = np\
+        .arccos(np.sin(np.deg2rad(_geo_df.lat))
+            * np.sin(np.deg2rad(f_lat))
+            + np.cos(np.deg2rad(_geo_df.lat))
+            * np.cos(np.deg2rad(f_lat))
+            * np.cos(np.deg2rad(f_lng)
+                        - (np.deg2rad(_geo_df.lng))
+                        )
+            ) * 6371
+    _geo_df.fdist = _geo_df.fdist.apply(lambda x: round(x,2))
+    # Reformat values
+    _added_d = _geo_df[['store_uuid', 'fdist']]\
+                .set_index('store_uuid')\
+                .to_dict()['fdist']
+    logger.info('Got distances')
+    return _added_d
+
+def date_js():
+    """ Convert datetime object into 
+        JS timestamp.
+
+        Returns
+        -----
+        f : lambda
+            Converter function to JS timestamp
+    """
+    return lambda djs: int((djs - 
+                        datetime.datetime(1970, 1, 1,0,0))\
+                        /datetime.timedelta(seconds=1)*1000)
+
+def grouping_periods(params):
+    """ Method the receives a dict params with the following keys:
+         
+        Params:
+        -----
+        params : dict
+            Dates info and interval
+        >>> { 
+            "date_ini": "(str) ISO format Date",
+            "date_fin": "(str) ISO format Date",
+            "interval": "(str) day | week | month"
+        }
+
+        Returns:
+        -----
+        groups : list
+            Group of valid date ranges
+    """
+    groups = []
+    di = datetime.datetime(*tuple(int(d) for d in params['date_ini'].split('-')))
+    df = datetime.datetime(*tuple(int(d) for d in params['date_fin'].split('-')))
+    # Day intervals
+    if params['interval'] == 'day':
+        groups.append([di])
+        while True:
+            di += datetime.timedelta(days=1)
+            groups.append([di])
+            if di >= df:
+                break
+    # Week intervals
+    elif params['interval'] == 'week':
+        groups.append([di,di+datetime.timedelta(days=7-di.isoweekday())])
+        di += datetime.timedelta(days=7-di.weekday())
+        while True:
+            if di >= df:
+                break
+            dv = di + datetime.timedelta(days=6)
+            groups.append([di,dv])
+            di = dv + datetime.timedelta(days=1)
+    # Monthly intervals
+    else:
+        lmd = calendar.monthrange(di.year,di.month)[1]
+        groups.append([di,di+datetime.timedelta(days=lmd-di.day)])
+        di += datetime.timedelta(days=lmd-di.day)
+        while True:
+            if di >= df:
+                break
+            _di_month = di.month + 1 if di.month != 12 else 1
+            _di_year = di.year + 1 if di.month == 12 else di.year
+            lmd = calendar.monthrange(_di_year,_di_month)[1]
+            dv = di + datetime.timedelta(days=lmd)
+            groups.append([di+ datetime.timedelta(days=1),dv])
+            di = dv
+    return groups
