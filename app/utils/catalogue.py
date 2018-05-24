d928b007d07848c38061f159e49e97e580b604f0
Rodrigo <gamba.lavin@gmail.com>
2018-05-24 15:49:45 -0500

    Add catalogue sdk items details method

--- app/utils/catalogue.py
+++ app/utils/catalogue.py
@@ -2,7 +2,37 @@ import requests
 
 class Catalogue(object):
 
-    def __init__(self):
-        """ 
+    def __init__(self, uri="", protocol="http"):
+        """ Constructor method
         """
-        pass
+        self.uri = uri or "geolocation"
+        self.protocol = protocol or "http"
+        self.base_url = "{}://{}".format(self.protocol, self.uri)
+        self.auth = None
+    
+    def get_item_details(
+        self, 
+        values=None, 
+        by='item_uuid', 
+        cols=['item_uuid','gtin','name'], 
+        loop_size=10
+        ):
+        """ Get item details by a given field 
+            that equals a given value.
+        """
+        if not values:
+            logger.error("Set the values to obtain")
+            return False
+        qry_cols = ','.join(cols)
+        # Get chunks of n size for the values
+        chunks = [values[i:i + loop_size] for i in range(0, len(values), loop_size)]
+        # Loop through chunks
+        result = []
+        for chunk in chunks:
+            qry_items=','.join(chunk)
+            # Make the request
+            items = requests.get(self.base_url+'/item/details').json()
+            result += items
+
+        return result
+        
