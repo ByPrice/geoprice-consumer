85f8f2cda9eb6b20d4ff243561e78f37afdab422
Rodrigo <gamba.lavin@gmail.com>
2018-07-02 16:47:53 -0500

    fix bin/run.sh app dir var

--- bin/run.sh
+++ bin/run.sh
@@ -3,8 +3,9 @@
 # if app dir not set, set to app name
 if [ -z "$APP_DIR" ]
     then
-    $APP_DIR='/'$APP_NAME
+    APP_DIR='/'$APP_NAME
 
+echo "Root app dir: $APP_DIR"
 cd $APP_DIR
 /bin/bash $APP_DIR/env/bin/activate
 
