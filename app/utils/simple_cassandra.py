0bcd480d2ee286fa1e959e2a01fdc45857785f0c
Krakkev <kevangy@hotmail.com>
2018-07-11 13:11:24 -0500

    trying another connection timeout

--- app/utils/simple_cassandra.py
+++ app/utils/simple_cassandra.py
@@ -41,14 +41,14 @@ class SimpleCassandra(object):
             self.cluster = Cluster(
                 self.config['CONTACT_POINTS'],
                 port=self.config['PORT'],
-                connect_timeout=30,
+                connect_timeout=100,
                 auth_provider=auth_provider
             )
         else:
             self.cluster = Cluster(
                 self.config['CONTACT_POINTS'],
                 port=self.config['PORT'],
-                connect_timeout=30
+                connect_timeout=100
             )
         # Set session
         try:
