f5824e4bee356911a667690644307f960766ec92
Adolfo Calderón <caveadolfo22@gmail.com>
2018-08-16 14:54:58 -0500

    endpoint modified to support streaming

--- app/controllers/promos.py
+++ app/controllers/promos.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, Response, stream_with_context
 from app.models.promos import Promos
 from app import errors, logger
+from app.models.item import Item
 import datetime
 
 mod = Blueprint('promos',__name__)
@@ -17,17 +18,16 @@ def main():
     })
 
 @mod.route('/daily', methods=['GET'])
-def get_promos_by_day():
+def generate_promos_by_day_stream():
     """
 		Return the promos applying that day, limited by the parameters
 		Params:
 		{
 			'day' : '2017-09-20'
-			'ip' : 1,
-			'ipp' : 500
+			'num_promos' : 20
 		}
 	"""
-	
+    logger.info("Promos daily stream endpoint!")
     if not request.args:
         raise errors.AppError(80002,"No params in request")
     if 'day' not in request.args:
@@ -37,15 +37,11 @@ def get_promos_by_day():
             datetime.datetime.strptime(request.args['day'], '%Y-%m-%d')
         except:
             raise errors.AppError(80012, "Incorrect data format, should be YYYY-MM-DD")
-    if 'ip' not in request.args or int(request.args['ip']) < 1:
-        ip = 1
+    if 'num_promos' not in request.args or int(request.args['num_promos']) < 1:
+        num_promos = 0
     else:
-        ip = int(request.args['ip'])
-    if 'ipp' not in request.args or int(request.args['ipp']) < 1:
-        ipp = 500
-    else:
-        ipp = int(request.args['ipp'])    
-    
-    logger.info("Promos by day endpoint "+request.args['day']+", "+str(ip)+", "+str(ipp))
-    promos = Promos.get_cassandra_promos_by_day(request.args['day'], ip, ipp)
-    return jsonify(promos)
+        num_promos = int(request.args['num_promos'])
+
+    def generate():
+        return Promos.get_cassandra_promos_by_day(request.args['day'], num_promos)
+    return Response(stream_with_context(generate()), content_type='application/json')
--- app/models/promos.py
+++ app/models/promos.py
@@ -1,11 +1,12 @@
 import datetime
 from uuid import UUID
 from io import StringIO
-from flask import g
+from flask import g, jsonify
 import pandas as pd
 from app import errors, logger
 from config import *
 from app.models.item import Item
+import json
 
 
 class Promos(object):
@@ -15,54 +16,75 @@ class Promos(object):
     """
     
     @staticmethod
-    def get_cassandra_promos_by_day(day, ip=1, ipp=500):
+    def get_cassandra_promos_by_day(day, max_num_promos=20):
         """ Query applying promos in that day
             
             Params:
             -----
             day :  str
                 day to look for promos
-            ip :  int
-                page number
-            ipp : int
-                number of results to look for
+            num_promos :  int
+                minimum number of promos to get
 
             Returns:
             -----
             promos : dict
                 JSON with applying promos
         """
+        CHUNK_SIZE = 500
+        num_found_promos = 0
+        ip=1
+        has_printed = False
         
-        CHUNK_SIZE = 500        
-        qs = []
-        
-        prods = Item.get_all_items(ip, ipp*2)
-        
-        for i in range(0, len(prods), CHUNK_SIZE):
-        
-            limited_prods = prods[i:i+CHUNK_SIZE]
+        yield '{"items": ['
+        while True:
+            #  get items to look for promos
+            prods = Item.get_all_items(ip, CHUNK_SIZE)
             
+            logger.info("Chunk {}"\
+                .format(ip))
+            
+            #  if no more items or we reached the limit, we're done
+            if len(prods) == 0 or (max_num_promos > 0 and num_found_promos >= max_num_promos):
+                logger.info("FINISHED FOUND {} Promos, exiting while..".format(num_found_promos))
+                break
+                
             # Fetch prod uuids
-            puuids = [p['product_uuid'] for p in limited_prods]
+            puuids = [p['product_uuid'] for p in prods]
             cass_query = """SELECT *
                     FROM promo
                     WHERE product_uuid IN {}
                     AND date = {}"""\
                         .format(str(tuple(_p for _p in puuids)).replace("'",""),
                                 day.replace("-",""))
-                                
+                                   
             try:
                 q = g._db.query(cass_query,
                     timeout=20)
                 if not q:
+                    ip += 1 
                     continue
-                qs += list(q)
+                qs = list(q)
             except Exception as e:
                 logger.error("Cassandra Connection error: " + str(e))
+                ip += 1 
                 continue
-        logger.info("Finished, found {} promos"\
-            .format(len(qs)))
         
-        return {
-            'promos': pd.DataFrame(list(qs)).to_dict(orient='records'),
-        }
+            num_found_promos += len(qs)
+            logger.info("Finished chunk {}, found {} promos, found {} total promos"\
+                .format(ip, len(qs), num_found_promos))
+            
+            #  create the DF and cast types
+            temp_df = pd.DataFrame(list(qs))
+            temp_df['product_uuid'] = temp_df['product_uuid'].astype('str')
+            temp_df['store_uuid'] = temp_df['store_uuid'].astype('str')
+            temp_df['time'] = temp_df['time'].astype('str')
+            for d in temp_df.to_dict(orient='records'):
+                if (has_printed):
+                    yield ', '+json.dumps(d)
+                else:
+                    yield json.dumps(d)
+            
+            has_printed = True    
+            ip += 1    
+        yield '], "message": "finished"}'
