caec26e8cb4273f30e4114b2ad693111af411d17
jorgeviz <javg44@hotmail.com>
2018-10-03 14:27:36 -0500

    Consumer updated to new parted schema

--- app/consumer.py
+++ app/consumer.py
@@ -17,6 +17,8 @@ logger = applogger.get_logger()
 
 q_geoprice = QUEUE_GEOPRICE
 q_cache = QUEUE_CACHE
+# Global counter
+gcounter = 1
 
 
 def with_context(original_function):
@@ -42,6 +44,7 @@ def with_context(original_function):
 
 # Rabbit MQ callback function
 def callback(ch, method, properties, body):
+    global gcounter
     try:
         new_price = json.loads(body.decode('utf-8'))
         logger.debug("Price "+new_price['retailer']+" - "+new_price['product_uuid']+" - stores: "+ str(len(new_price['location']['store'])))
@@ -51,11 +54,19 @@ def callback(ch, method, properties, body):
             pass
         else:
             price = Price(new_price)
+            # Set Partition value
+            price.part = gcounter
+            # Save elements
             price.save_all()
             logger.info('Saved price for ' + price.retailer + ' ' + str(price.product_uuid))
             # Publish message to price-cache
             if q_cache in g._producer and  g._producer[q_cache]:
                 g._producer[q_cache].publish_message(q_cache, new_price)
+                # Modify partition to distribute
+                if gcounter >= 20:
+                    gcounter = 1
+                else:
+                    gcounter += 1
             else:
                 logger.warning("Producer not initialized!")
                 logger.error(g._producer)
