23bb1ef38b5683563bb445a6b4e37959c6c68a55
Adolfo Calderón <caveadolfo22@gmail.com>
2018-11-15 16:21:39 -0600

    corrected comma in yield

--- app/models/promos.py
+++ app/models/promos.py
@@ -14,11 +14,11 @@ class Promos(object):
         on Cassandra to get the
         applying promos for that day.
     """
-    
+
     @staticmethod
     def get_cassandra_promos_by_day(day, max_num_promos=20):
         """ Query applying promos in that day
-            
+
             Params:
             -----
             day :  str
@@ -33,47 +33,48 @@ class Promos(object):
         """
         CHUNK_SIZE = 500
         num_found_promos = 0
-        ip=1
+        ip = 1
         has_printed = False
-        
+
         yield '{"items": ['
         while True:
             #  get items to look for promos
             prods = Item.get_all_items(ip, CHUNK_SIZE)
-            
-            logger.info("Chunk {}"\
-                .format(ip))
-            
+
+            logger.info("Chunk {}"
+                        .format(ip))
+
             #  if no more items or we reached the limit, we're done
             if len(prods) == 0 or (max_num_promos > 0 and num_found_promos >= max_num_promos):
-                logger.info("FINISHED FOUND {} Promos, exiting while..".format(num_found_promos))
+                logger.info(
+                    "FINISHED FOUND {} Promos, exiting while..".format(num_found_promos))
                 break
-                
+
             # Fetch prod uuids
             puuids = [p['product_uuid'] for p in prods]
             cass_query = """SELECT *
                     FROM promo
                     WHERE product_uuid IN {}
                     AND date = {}"""\
-                        .format(str(tuple(_p for _p in puuids)).replace("'",""),
-                                day.replace("-",""))
-                                   
+                        .format(str(tuple(_p for _p in puuids)).replace("'", ""),
+                                day.replace("-", ""))
+
             try:
                 q = g._db.query(cass_query,
-                    timeout=20)
+                                timeout=20)
                 if not q:
-                    ip += 1 
+                    ip += 1
                     continue
                 qs = list(q)
             except Exception as e:
                 logger.error("Cassandra Connection error: " + str(e))
-                ip += 1 
+                ip += 1
                 continue
-        
+
             num_found_promos += len(qs)
-            logger.info("Finished chunk {}, found {} promos, found {} total promos"\
-                .format(ip, len(qs), num_found_promos))
-            
+            logger.info("Finished chunk {}, found {} promos, found {} total promos"
+                        .format(ip, len(qs), num_found_promos))
+
             #  create the DF and cast types
             temp_df = pd.DataFrame(list(qs))
             temp_df['product_uuid'] = temp_df['product_uuid'].astype('str')
@@ -84,7 +85,7 @@ class Promos(object):
                     yield ', '+json.dumps(d)
                 else:
                     yield json.dumps(d)
-            
-            has_printed = True    
-            ip += 1    
+                has_printed = True
+
+            ip += 1
         yield '], "message": "finished"}'
