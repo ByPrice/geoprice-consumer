b9be3e2d1b963eb61fbab5681f536ebe6b2e3bbe
Jorge Vizcayno <javg44@hotmail.com>
2019-05-29 12:00:10 -0500

    Geo Historia Submit task

--- app/models/geo_historia.py
+++ app/models/geo_historia.py
@@ -6,11 +6,13 @@ import config
 import requests
 import json
 import pandas as pd
-from app.models.geo_mapa import Map
+from app.models.geo_mapa import Map, grouping_cols
 from app.models.task import Task
+from app.models.price import Price
 
 # Logger
 logger = applogger.get_logger()
+
 
 class Historia(Map):
     """ Class to perform Historic needed routines for Geo located prices,
@@ -91,13 +93,106 @@ class Historia(Map):
         except Exception as e:
             logger.error(e)
             return []
+    
+    @staticmethod
+    def decompose_filters(task, filters, rets, date_start, date_end):
+        """ Given filters with time intervals, stores, retailers and items,
+            return decomposed elements for querying ing C*
+
+            Params:
+            -----
+            - task: (app.models.Task)  Task instance
+            - filters: (list) Item Filters
+            - rets: (list) Needed Retailers
+            - date_start: (str) Date Lower Bound
+            - date_from: (str) Date Upper Bound
+
+            Returns:
+            -----
+            (tuple): Tuple containing the following
+                (dates, retailers, retailers_by_key, stores, 
+                stores_by_uuid, items, items_by_uuid,
+                prods, prods_by_uuid)
+        """
+        # Filters
+        f_retailers = [ f['retailer'] for f in filters if 'retailer' in f ]
+        f_stores = [ f['store_uuid'] for f in filters if 'store_uuid' in f ]
+        f_items = [ f['item_uuid'] for f in filters if 'item_uuid' in f ]
+        logger.debug("Filters for the task: {}".format(filters))
+        # Dates
+        dates = []
+        date_start = moment = datetime.datetime.strptime(date_start, "%Y-%m-%d") 
+        date_end = datetime.datetime.strptime(date_end, "%Y-%m-%d") 
+        while moment <= date_end:
+            dates.append(int(moment.strftime("%Y%m%d")))
+            moment = moment + datetime.timedelta(days=1)
+        logger.debug("Dates list: {}".format(dates))
+
+        # Get retailers and its details
+        retailers = rets if type(rets) == dict else { r['key'] : r for r in rets }
+        if f_retailers:
+            retailers_by_key = { k : v for k,v in retailers.items() if k in f_retailers }
+        else:
+            retailers_by_key = retailers 
+
+        # Get all stores
+        stores = g._geolocation.get_stores(retailers_by_key.keys())
+        # Stores by their uuid
+        if f_stores:
+            stores_by_uuid = { s['uuid'] : s for s in stores if s['uuid'] in f_stores }
+        else:
+            stores_by_uuid = { s['uuid'] : s for s in stores }
+
+        # Get details of all items requested
+        items = g._catalogue.get_items_details(
+            values=f_items, 
+            cols=['item_uuid','gtin','name']
+        )
+        items_by_uuid = { i['item_uuid'] : i for i in items }
+        task.progress = 20
+
+        # Get products per item and then prices to build the main DF
+        prods = []
+        for i,it in enumerate(items):
+            # Get product of the item
+            prods += g._catalogue.get_intersection(
+                item_uuid=[it['item_uuid']], 
+                source=list(retailers_by_key.keys()),
+                cols=['item_uuid','product_uuid','source','gtin','name'],
+            )
+        # All prods by uuid
+        prods_by_uuid = { p['product_uuid'] : p for p in prods }
+        logger.info("Got products from filters")
+        task.progress = 35
+        # Return elements
+        return dates, retailers, retailers_by_key, \
+            stores, stores_by_uuid, items, \
+            items_by_uuid, prods, prods_by_uuid
+    
+    @staticmethod
+    def add_dates(df):
+        """ Method to add dates to DF
+
+            Params:
+            ------
+            - df : (pandas.DataFrame) Data Set
+
+            Returns:
+            -----
+            (pandas.DataFrame)  Data Set with Date Columns
+        """        
+        df['day'] = df['time'].apply(lambda x: x.day)
+        df['month'] = df['time'].apply(lambda x: x.month)
+        df['year'] = df['time'].apply(lambda x: x.year)
+        df['week'] = df['time'].apply(lambda x: x.isocalendar()[1])
+        return df
 
     @staticmethod
     def grouped_by_retailer(task_id, filters, rets, _from, until, interval):
         """ Static method to retrieve passed prices by all 
             available stores given UUIDS and retailer keys
             from previous 24 hrs. 
-            Result is temporarly stored in dumps/<task_id>
+            Result is temporarly stored in Redis
 
             Params:
             -----
@@ -113,43 +208,35 @@ class Historia(Map):
         task.task_id = task_id
         task.progress = 1
         
-        # Validate Params
-        filters += [{'retailer': _ret} for _ret in rets.keys()]
-        print(filters)
-
-        task.progress = 100
-        return {
-            'data': [],
-            'msg': 'Task test OK'
-        }
-        ##
-        ##
-
-        # Fetch Items by filters
-        items = Mapa.fetch_from_item(filters)
-        Historia.write_file('states', task_id, '10')
-        # Fetch stores by retailers
-        stores = Mapa.fetch_stores([r for r in rets.keys()])
-        # Create Stores DF
-        stores_df = pd.DataFrame(stores)
-        stores_df.rename(columns={'name': 'store'}, inplace=True)
-        stores_df['store_uuid'] = stores_df.uuid.apply(lambda x: str(x))
-        Historia.write_file('states', task_id, '20')
-        # Start fetching prices
-        hist_prices = Historia.fetch_prices([i['item_uuid'] for i in items],
-                                        _from,
-                                        until)
-        if not hist_prices:
-            # Put an empty result
-            Historia.write_file('states', task_id, '100')
-            Historia.write_file('dumps', task_id, '')
-            raise Exception('Could not fetch Prices!')
-        # Update loading status
-        Historia.write_file('states', task_id, '50')
-        # Load DF
-        hist_df = pd.DataFrame(hist_prices)
-        hist_df.item_uuid = hist_df.item_uuid.apply(lambda x: str(x))
-        hist_df.store_uuid = hist_df.store_uuid.apply(lambda x: str(x))
+        # Decompose filters
+        dates, retailers, \
+            retailers_by_key, \
+            stores, stores_by_uuid, \
+            items, items_by_uuid, \
+            prods, prods_by_uuid = Historia.decompose_filters(
+                                task,
+                                filters, 
+                                rets, 
+                                _from,
+                                until
+                            )
+        logger.info("Got products from filters")
+        # Get prices of all the products
+        prices = Price.query_by_product_store(
+            stores=list(stores_by_uuid.keys()),
+            products=list(prods_by_uuid.keys()),
+            dates=dates
+        )
+        task.progress = 60
+        # If no prices, end task...
+        if not prices:
+            logger.warning("No prices found!")
+            raise Exception("No se encontraron precios, intenta nuevamente")
+        # Format prices
+        hist_df = pd.DataFrame(prices)
+        hist_df['product_uuid'] = hist_df.product_uuid.apply(lambda x: str(x))
+        hist_df['store_uuid'] = hist_df.store_uuid.apply(lambda x: str(x))
+        hist_df['item_uuid'] = hist_df.product_uuid.apply(lambda z: prods_by_uuid[z]['item_uuid'])
         # Add JS-like timestamp
         hist_df.sort_values(['time'], ascending=False, inplace=True)
         hist_df['time_js'] = hist_df.time\
@@ -163,40 +250,15 @@ class Historia(Map):
         hist_df.drop_duplicates(['item_uuid', 'store_uuid', 'date'],
                                 keep='first',
                                 inplace=True)
-        logger.info('Droping duplicates and starting to format..')
-        # Update loading status
-        Historia.write_file('states', task_id, '60')
-        # Filter only Valid Retailers
-        hist_df = hist_df[hist_df['retailer']\
-                .apply(lambda x: True if x in rets \
-                                      else False) == True]
-        # Filter only Valid Stores
-        if ('store' in [list(_fil.keys())[0] for _fil in filters]):
-            _stuids = [_su['store'] for _su in filters \
-                        if 'store' in _su]
-            hist_df = hist_df[hist_df.store_uuid\
-                        .apply(lambda x: True \
-                                if x in _stuids \
-                                else False) 
-                        == True]
-        # Complete Item names and Store Names
-        items_df = pd.DataFrame(items)
-        items_df.item_uuid = items_df.item_uuid.apply(lambda x: str(x))
-        hist_df = pd.merge(hist_df,
-                        items_df,
-                        on='item_uuid',
-                        how='left')
-        hist_df = pd.merge(hist_df,
-                        stores_df[['store_uuid','store']],
-                        on='store_uuid',
-                        how='left')
-        # Add Correct Retailer names
-        hist_df.retailer = hist_df.retailer.apply(lambda x: rets[x])
+        task.progress = 70
+        # Add name and gtin
+        hist_df['gtin'] = hist_df.item_uuid.apply(lambda z: items_by_uuid[z]['gtin'])
+        hist_df['name'] = hist_df.product_uuid.apply(lambda z: prods_by_uuid[z]['name'])
+        hist_df['store'] = hist_df.store_uuid.apply(lambda z: stores_by_uuid[z]['name'])
+        hist_df['retailer'] = hist_df.retailer.apply(lambda z: retailers_by_key[z])
         # Add Grouping dates
         hist_df = Historia.add_dates(hist_df)
-        logger.info('Finished format, writing result..')
-        Historia.write_file('states', task_id, '80')
-        # Format Result
+        task.progress = 80
         # Add Retailer Format
         ret_res = []
         for _gk, _gdf in hist_df.groupby(['retailer']):
@@ -204,19 +266,18 @@ class Historia(Map):
                 "name": _gk, "data": []
             }
             # Once grouped by retailer aggregate by interval
-            for _dk, _ddf in _gdf.groupby(Historia\
-                                    .grouping_cols[interval]):
+            for _dk, _ddf in _gdf.groupby(grouping_cols[interval]):
                 _tmp['data'].append([
                         int(_ddf.time_js.min()),
                         round(_ddf.price.mean(), 2)
                     ])
             # Append to result
             ret_res.append(_tmp)
+        task.progress = 90
         logger.info('Created Retailer result')
         # Add Metrics Format
         met_res = {'avg': [], 'min': [], 'max': []}
-        for mj, mrow in hist_df.groupby(Historia\
-                                    .grouping_cols[interval]):
+        for mj, mrow in hist_df.groupby(grouping_cols[interval]):
             # Take JS Timestamp
             _tmpjs = int(mrow.time_js.min())
             met_res['avg'].append([_tmpjs, round(mrow.price.mean(), 2)])
@@ -235,9 +296,11 @@ class Historia(Map):
                         ', '.join([z['name'] for z in ret_res])
                     )
         }
-        Historia.write_file('states', task_id, '100')
-        Historia.write_file('dumps', task_id, json.dumps(result))
-        logger.info('Finished computing {}!'.format(task_id))
+        task.progress = 100
+        return {
+            'data': result,
+            'msg': 'Task test OK'
+        }
         
 
 
--- app/models/geo_mapa.py
+++ app/models/geo_mapa.py
@@ -127,7 +127,6 @@ class Map(object):
         f_stores = [ f['store_uuid'] for f in filters if 'store_uuid' in f ]
         f_items = [ f['item_uuid'] for f in filters if 'item_uuid' in f ]
         logger.info("Filters for the task: {}".format(filters))
-
         # Dates
         dates = []
         date_start = moment = datetime.datetime.strptime(date_start, "%Y-%m-%d") 
