ab1688ddda1b8c365768c7ba30271ed22793342f
Jorge Vizcayno <javg44@hotmail.com>
2019-06-05 01:54:19 -0500

    Improved memory performance in stats

--- app/scripts/create_stats.py
+++ app/scripts/create_stats.py
@@ -25,7 +25,8 @@ from tqdm import tqdm
 
 # Logger
 logger = applogger.get_logger()
-
+# Number of Batches to separate data
+NUM_BATCHES = 100
 
 def stats_args():
     """ Parse Create Stats Script args
@@ -66,77 +67,129 @@ def get_daily_data(_day):
 
         Returns:
         -----
-        daily : pd.DataFrame
-            Table of daily prices data 
+        _tfiles : list
+            List of TMP file paths to generate stats
     """
     logger.info('Successfully connected to C*')
-    daily = []
     stores = get_all_stores()
     cass_qry = """SELECT product_uuid, price, date, source
         FROM price_by_store WHERE date = %s AND store_uuid = %s
     """
     _day = int(_day.isoformat().replace('-', ''))
-    for _st in tqdm(stores.store_uuid.tolist(), desc="Store Prices"):
-        try:
-            q = g._db.query(cass_qry, (_day, UUID(_st)), timeout=200)
-            if not q:
+    _daily_count, st_list, _tfiles = 0, stores.store_uuid.tolist(), []
+    # Generate N tmp files depending on the param
+    for _j in range(0, len(stores), NUM_BATCHES):
+        daily = []
+        for _st in tqdm(st_list[_j:_j+NUM_BATCHES], desc="Store Prices"):
+            try:
+                q = g._db.query(cass_qry, (_day, UUID(_st)), timeout=200)
+                if not q:
+                    continue
+                daily += list(q)
+            except Exception as e:
+                logger.error("Cassandra Connection error: " + str(e))
                 continue
-            daily += list(q)
-        except Exception as e:
-            logger.error("Cassandra Connection error: " + str(e))
+        if not daily:
             continue
-    # Format Data
-    if not daily:
-        return pd.DataFrame()
-    daily = pd.DataFrame(daily)
-    #daily['product_uuid'] = daily['product_uuid'].astype(str)
-    logger.info("Found {} daily prices".format(len(daily)))
+        # Generate TMP csv file  
+        _tfile_name = BASE_DIR+'/data/tmp_{}_{}.csv'.format(_day, _j)
+        pd.DataFrame(daily).to_csv(_tfile_name)
+        _daily_count += len(daily)
+        _tfiles.append(_tfile_name)
+        logger.info("Created: " + _tfile_name)
+    logger.info("Found {} daily prices".format(_daily_count))
     # Return
-    return daily
+    return _tfiles
 
 
-def aggregate_daily(daily):
+def aggregate_daily(daily_files):
     """ Aggregate data to compute statistics
         and batch load it in to C* table
 
         Params:
         -----
-        daily : pd.DataFrame
-            Daily prices data
-        max_batch : int
-            Max batch size to load into C*
+        daily : list
+            Daily prices tmp files 
     """
     def _mode(x):
         try:
             return stats.mode(x)[0][0]
         except:
             return np.median(x)
-    # Aggregate data to compute mean, std, max, min, etc.
-    aggr_stats = daily.groupby(['product_uuid', 'date']).price\
-        .agg([('max_price', 'max'),
-              ('avg_price', 'mean'),
-              ('min_price', 'min'),
-              ('datapoints', 'count'),
-              ('std_price', 'std'),
-              ('mode_price', lambda x: _mode(x))
-              ])
-    aggr_stats.fillna(0.0, inplace=True)
-    aggr_stats.reset_index(inplace=True)
-    # Add retailer
-    aggr_stats = pd.merge(
-        aggr_stats,
-        daily[['product_uuid', 'source']].drop_duplicates('product_uuid'),
-        on='product_uuid',
-        how='left'
+    # File name acumulator
+    agg_files = []
+    # Compute and reduce number of rows
+    for dfile in daily_files:
+        daily = pd.read_csv(dfile, low_memory=False)
+        # Aggregate data to compute mean, std, max, min, etc.
+        aggr_stats = daily.groupby(['product_uuid', 'date']).price\
+            .agg([('max_price', 'max'),
+                ('avg_price', 'mean'),
+                ('min_price', 'min'),
+                ('datapoints', 'count'),
+                ('std_price', 'std'),
+                ('mode_price', lambda x: _mode(x))
+                ])
+        aggr_stats.fillna(0.0, inplace=True)
+        aggr_stats.reset_index(inplace=True)
+        # Add retailer
+        aggr_stats = pd.merge(
+            aggr_stats,
+            daily[['product_uuid', 'source']].drop_duplicates('product_uuid'),
+            on='product_uuid',
+            how='left'
+        )
+        # Write agg file
+        fagg_name = dfile.replace('.csv','_agg.csv')
+        aggr_stats.to_csv(fagg_name)
+        # Accumulate agg files
+        agg_files.append(fagg_name)
+    logger.info("Computed all mini batch aggregates!")
+    # Read all agg files and compute
+    daily_aggs = pd.concat([pd.read_csv(af, low_memory=False) for af in agg_files])
+    def compute_overall_stats(z):
+        """ Calculate stats of stats
+        """
+        def _mode(x):
+            try:
+                return stats.mode(x)[0][0]
+            except:
+                return np.median(x)
+        return pd.Series({
+            "max_price": z.max_price.max(),
+            "min_price": z.min_price.min(),
+            "avg_price": (z.avg_price * z.datapoints).mean(),
+            "datapoints": z.datapoints.sum(),
+            "std_price": (z.std_price * z.std_price * z.datapoints).mean(),
+            "mode_price": _mode(z.mode_price.tolist())
+        })
+    # Aggregate data to compute max
+    all_aggr_stats = daily_aggs\
+        .groupby(['product_uuid', 'date'])\
+        .apply(compute_overall_stats)\
+        .fillna(0.0)\
+        .reset_index()
+    # Aggregate data, add retailer
+    all_aggr_stats = pd.merge(
+        all_aggr_stats,
+        daily_aggs[['product_uuid', 'source']].drop_duplicates('product_uuid'),
+        on="product_uuid",
+        how="left"
     )
+    print(all_aggr_stats.head())
+    input("BREAKPOINT")
     # Load each element into C*
-    for elem in tqdm(aggr_stats.to_dict(orient='records'), desc="Writing.."):
+    for elem in tqdm(all_aggr_stats.to_dict(orient='records'), desc="Writing.."):
         Price.save_stats_by_product(elem)
     # Disply metrics
-    logger.info("Stored {} daily prices".format(len(aggr_stats)))
-    logger.info("Prices had the following distribution:\n{}"
-                .format(aggr_stats.datapoints.describe()))
-
+    logger.info("Stored {} daily prices".format(len(all_aggr_stats)))
+    logger.info("Prices had the following distribution:\n {}"
+                .format(all_aggr_stats.datapoints.describe()))
+    # Delete all temp_fils
+    for taf in agg_files:
+        os.remove(taf)
+    for taf in daily_files:
+        os.remove(taf)
 
 @with_context
 def daily_stats(_day):
@@ -150,12 +203,12 @@ def daily_stats(_day):
             Max batch size to load into C*
     """
     # Retrieve daily data
-    daily = get_daily_data(_day)
+    daily_files = get_daily_data(_day)
     # Aggregate data and load into C* table
-    if daily.empty:
+    if not daily_files:
         logger.warning("No prices available!!")
         return
-    aggregate_daily(daily)
+    aggregate_daily(daily_files)
 
 def start():
     """ Start Method for `flask script --name=<script>` command
