0249cf189d79610cae873d3ee5f346f82939254e
Jorge Vizcayno <javg44@hotmail.com>
2019-06-08 18:52:47 -0500

    removed indication elements

--- app/models/price.py
+++ app/models/price.py
@@ -562,9 +562,7 @@ class Price(object):
             `promo_by_store`
         """ 
         try:
-            print("Before execution!")
             elem = list(self.loc_generator())[0]
-            print("POST execution")
             self.session.execute(
                 """
                 BEGIN BATCH
