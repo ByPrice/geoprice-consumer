648caa73cbf7ec7a28c500cf2b8a0d49cf6fe3d2
Jorge Vizcayno <javg44@hotmail.com>
2019-06-21 01:34:00 -0500

    Updated sh files

--- bin/create_backups.sh
+++ bin/create_backups.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Script to run backups in server
-
+/home/byprice/geoprice/
 # Env
 source /home/byprice/geoprice/.envvars
 source /home/byprice/geoprice/env/bin/activate
 
 # Run script
 echo "Running Create Backups .."
-/home/byprice/geoprice/env/bin/flask script create_backups
+/home/byprice/geoprice/env/bin/flask script --name=create_backups
