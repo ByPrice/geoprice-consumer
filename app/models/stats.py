58e6afdfb4b76b19c2865a693ada7257933cb2af
Adolfo Calderón <caveadolfo22@gmail.com>
2019-10-07 11:43:54 -0500

    corrected bracket

--- app/models/stats.py
+++ app/models/stats.py
@@ -454,7 +454,7 @@ class Stats(object):
         df_curr = Stats\
             .get_cassandra_by_ret(filt_items,
                                   rets,
-                                  [date_start, date_end)\
+                                  [date_start, date_end])\
             .sort_values(by=['date'], ascending=False)\
             .drop_duplicates(subset=['product_uuid'], keep='first')  # today
         df_curr.rename(columns={'avg_price': 'avg',
