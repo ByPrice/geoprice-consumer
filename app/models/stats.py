f1bd8e21ffde57f0c1cee9a3f8b605986c05e630
Adolfo Calderón <caveadolfo22@gmail.com>
2020-01-06 17:16:10 -0600

    removed drop duplicates for quick fix

--- app/models/stats.py
+++ app/models/stats.py
@@ -851,7 +851,7 @@ class Stats(object):
         items_df = pd.DataFrame(items_details)
 
         tmp_df = pd.merge(formatted_df, items_df, on="item_uuid")
-        tmp_df.drop_duplicates(subset='item_uuid', inplace=True)
+        #tmp_df.drop_duplicates(subset='item_uuid', inplace=True)
         formatted = tmp_df.to_dict(orient='records')                           
 
         return {"data": formatted, "msg": "Task completed"}
