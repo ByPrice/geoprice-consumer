fa5ba030ca901b5a34364fa48b7950755bfd2e84
Adolfo Calderón <caveadolfo22@gmail.com>
2020-05-18 15:52:06 -0500

    removed more common log in consumer

--- app/controllers/history_product.py
+++ app/controllers/history_product.py
@@ -53,8 +53,8 @@ def get_today_prices_bystore():
     prod = Product.get_by_store(item_uuid,
         product_uuid, lat, lng, radius)
     if not prod:
-        logger.warning("No prices in queried {} {}!".format('item' if item_uuid else 'product',\
-            item_uuid if item_uuid else product_uuid))
+        '''logger.warning("No prices in queried {} {}!".format('item' if item_uuid else 'product',\
+            item_uuid if item_uuid else product_uuid))'''
         return jsonify([])
     logger.info('Found {} prices'.format(len(prod)))
     logger.debug("Response prices:")
@@ -82,8 +82,8 @@ def get_history_prices_bystore():
     # Call to fetch prices
     prod = Product.get_history_by_store(item_uuid, product_uuid, period)
     if not prod:
-        logger.warning("No prices in queried {} {}!".format('item' if item_uuid else 'product',\
-            item_uuid if item_uuid else product_uuid))
+        '''logger.warning("No prices in queried {} {}!".format('item' if item_uuid else 'product',\
+            item_uuid if item_uuid else product_uuid))'''
         return jsonify({})
     logger.info('Found {} metrics'.format(len(prod['history'])))
     return jsonify(prod)
